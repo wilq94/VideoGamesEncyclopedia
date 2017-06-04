bc406ae0c54c696fc56a1d389c197af795b41964
wilq94 <paba.lech@poczta.fm>
2017-06-04 03:16:04 +0200

    First work on UserPanel

---
+++ VideoGamesEncyclopedia/Controllers/UserPanelController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoGamesEncyclopedia.Controllers
+{
+    public class UserPanelController : Controller
+    {
+        // GET: UserPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditProfile()
+        {
+            return View();
+        }
+
+        public ActionResult WishList()
+        {
+            return View();
+        }
+
+        public ActionResult IgnoredList()
+        {
+            return View();
+        }
+
+        public ActionResult RatedList()
+        {
+            return View();
+        }
+    }
+}
